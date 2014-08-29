6959bf8638d04cfa05d34574d296d03b045c358c
Ariel Kuhn <abpoker01@gmail.com>
2014-08-29 09:53:10 -0300

    155

--- partials/content-curso.php
+++ partials/content-curso.php
@@ -1,12 +1,12 @@
 
 						
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-
+	
 	<?php for($i=1;$i<6;$i++){?>
 
 	<?php $link =sanitize_title(get_field('titulo_menu_'.$i));?>
 	<a name="<?php echo $link.$i?>"></a>
-	<h2 data-magellan-destination="<?php echo $link.$i?>"><?php the_field('titulo-'.$i);?></h2>
+	<h1 data-magellan-destination="<?php echo $link.$i?>"><?php the_field('titulo-'.$i);?></h1>
 	<?php the_field('contenido-'.$i);?>
 	<?php the_field('contenido-'.$i);?>
 
