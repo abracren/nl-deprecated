66c6ffcc22d23e058edce1aabc5e34bf9ecd7b8f
Ariel Kuhn <abpoker01@gmail.com>
2014-09-01 13:49:33 -0300

    172

--- page-cursos-express.php
+++ page-cursos-express.php
@@ -14,7 +14,9 @@ Template Name: Cursos Express
 				    <div id="main" class="large-12 medium-12 columns first clearfix" role="main">
 
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
+
+					    	<div class="row all">
+
 					    	<?php get_template_part( 'partials/loop', 'page' ); ?>
 					    	  <?php if( have_rows('curso') ):
 
@@ -23,64 +25,65 @@ Template Name: Cursos Express
 
 							        // display a sub field value
 							        ?>
-							      <div class="row all">
+							        <?php if($count==0){?>
+							     	<div class="row up hide-for-small ">
+
+									<div class="small-12 columns  wideCurseRow" >
 
 
-									<div class="row">
-										<div class="small-12 columns wideCurse" >
-												<img src="<?php the_sub_field('imagen');?>"/>
+										<div class="row ">
+											<div class="small-12 columns wideCurse" >
+													<img src="<?php the_sub_field('imagen');?>"/>
 
 
-												<div class="wideCurseIn small-6 columns">
-													<h1><?php the_sub_field('titulo');?></h1>
-													<?php the_sub_field('descripcion');?>
-													<div class="requisitos">Requisitos Mínimos para realizar el curso: <?php the_sub_field('nivel_minimo');?></div>
+													<div class="wideCurseIn small-6 columns">
+														<h1><?php the_sub_field('titulo');?></h1>
+														<?php the_sub_field('descripcion');?>
+														<div class="requisitos">Requisitos Mínimos para realizar el curso: <?php the_sub_field('nivel_minimo');?></div>
+													</div>
+											</div>
+										</div>
+
+
+
+
+
+
+										<div class="row">
+											<div class="small-12 columns wideCurse_footer">
+												<div class="small-8 columns wideCurseLeft">
+
+													<p>Fecha: <?php the_sub_field('fecha');?></p>
+													<p>Horario: <?php the_sub_field('horario');?></p>
+
+													<p>Lugar: <?php the_sub_field('lugar');?></p>
 												</div>
+												<div class="small-1 columns wideCurseCenter ">
+													 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
+													</div>
+												<div class="small-3 columns wideCurseRight ">
+												       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
+												       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
+
+
+												</div>
+											</div>
 										</div>
 									</div>
 
 
-
-
-
-
-									<div class="row">
-										<div class="small-12 columns wideCurse_footer">
-											<div class="small-8 columns wideCurseLeft">
-
-												<p>Fecha: <?php the_sub_field('fecha');?></p>
-												<p>Horario: <?php the_sub_field('horario');?></p>
-
-												<p>Lugar: <?php the_sub_field('lugar');?></p>
-											</div>
-											<div class="small-1 columns wideCurseCenter ">
-												 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
-												</div>
-											<div class="small-3 columns wideCurseRight ">
-											       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
-											       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
-
-
-											</div>
-										</div>
-									</div>
 								</div>
-								</div>
-
-							<!-- SECOND -->
-
-
 								
+								<!-- SECOND -->
 
-
-								<div class="row">
-									  <div class="small-12 medium-12 large-12 columns"></div>
+								<div class="row hide-for-medium-up">
+									<div class="small-12 medium-12 large-12 columns"></div>
 									
 										<div class="row">
-										  <div class="small-12 medium-6 large-6 columns thinCurse ">
-										  	<div class="expressHeaderImageSmall	">
-												<img  class=""src="<?php the_sub_field('imagen');?>"/>
-											</div>
+											  <div class="small-12 medium-6 large-6 columns thinCurse ">
+											  	<div class="expressHeaderImageSmall	">
+													<img  class=""src="<?php the_sub_field('imagen');?>"/>
+												</div>
 												<div class="wideCurseIn small-12 columns">
 													<h3><?php the_sub_field('titulo');?></h3>
 													<?php the_sub_field('descripcion');?>
@@ -90,65 +93,90 @@ Template Name: Cursos Express
 												<div class="small-12 columns wideCurse_footer">
 													<div class="small-6 columns wideCurseLeft">
 
-														<p>Fecha: <?php the_sub_field('fecha');?></p>
-														<p>Horario: <?php the_sub_field('horario');?></p>
+															<p>Fecha: <?php the_sub_field('fecha');?></p>
+															<p>Horario: <?php the_sub_field('horario');?></p>
 
-														<p>Lugar: <?php the_sub_field('lugar');?></p>
+															<p>Lugar: <?php the_sub_field('lugar');?></p>
 													</div>
 													<div class="small-1 columns wideCurseCenter ">
-														 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
-														</div>
+															 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
+															</div>
 													<div class="small-5 columns wideCurseRight ">
-													       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
-													       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
+														       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
+														       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
 
 
 													</div>
 												</div>
 
-										  </div>
+											 </div>
 
 
 
-										  <!-- SECOND COLUMNS -->
-										  <div class="small-12 medium-6 large-6 columns thinCurse ">
-										  	<div class="expressHeaderImageSmall	">
-												<img  class=""src="<?php the_sub_field('imagen');?>"/>
-											</div>
-												<div class="wideCurseIn small-12 columns">
-													<h3><?php the_sub_field('titulo');?></h3>
-													<?php the_sub_field('descripcion');?>
-													<div class="requisitos">Requisitos Mínimos para realizar el curso: <?php the_sub_field('nivel_minimo');?></div>
-												</div>
-
-												<div class="small-12 columns wideCurse_footer">
-													<div class="small-6 columns wideCurseLeft">
-
-														<p>Fecha: <?php the_sub_field('fecha');?></p>
-														<p>Horario: <?php the_sub_field('horario');?></p>
-
-														<p>Lugar: <?php the_sub_field('lugar');?></p>
-													</div>
-													<div class="small-1 columns wideCurseCenter ">
-														 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
-														</div>
-													<div class="small-5 columns wideCurseRight ">
-													       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
-													       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
-
-
-													</div>
-												</div>
-
-										  </div>
+					
 										 
 										</div>
 									</div>
 
+								<?php }else{ ?>
+								<?php if($count==1){?>
+								<div class="row ">
+									<div class="small-12 medium-12 large-12 columns"></div>
+									<?php } ?>
+									
+										<div class="row">
+											  <div class="small-12 medium-6 large-6 columns thinCurse ">
+											  	<div class="expressHeaderImageSmall	">
+													<img  class=""src="<?php the_sub_field('imagen');?>"/>
+												</div>
+												<div class="wideCurseIn small-12 columns">
+													<h3><?php the_sub_field('titulo');?></h3>
+													<?php the_sub_field('descripcion');?>
+													<div class="requisitos">Requisitos Mínimos para realizar el curso: <?php the_sub_field('nivel_minimo');?></div>
+												</div>
 
+												<div class="small-12 columns wideCurse_footer">
+													<div class="small-6 columns wideCurseLeft">
+
+															<p>Fecha: <?php the_sub_field('fecha');?></p>
+															<p>Horario: <?php the_sub_field('horario');?></p>
+
+															<p>Lugar: <?php the_sub_field('lugar');?></p>
+													</div>
+													<div class="small-1 columns wideCurseCenter ">
+															 <img class="imageBlock" src="<?php echo get_template_directory_uri(); ?>/imagesRoot/diagonal-ex-foot.png"></img>
+															</div>
+													<div class="small-5 columns wideCurseRight ">
+														       <div class="precioExpress"><?php the_sub_field('precio');?><span class="euroSymbolExpress">€</span></div>
+														       <a><img src="<?php echo get_template_directory_uri(); ?>/imagesRoot/paypal2.png" border="0" alt="Marcas de aceptación" /></a>
+
+										
+
+													</div>
+												</div>
+
+											 </div>
+
+
+
+					
+										
+										 <?php if($count==2){?>
+										</div>
+									</div>
+								</div>
+							<?php 
+							$count=0;
+							} ?>
+								<?php
+
+							
+
+
+
+								} ?>
 							       
 							       
-							        
 							        
 
 								
@@ -165,6 +193,10 @@ Template Name: Cursos Express
 								endif;
 
 								?>
+															</div>
+
+							</div> <!-- endROWaLL -->
+
 					    					
 					    <?php endwhile; else : ?>
 					
